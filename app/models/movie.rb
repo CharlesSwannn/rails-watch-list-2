9e019d09a3cdf7dc71d6ca2d1577001be0b43700
André Marques <115092696+CharlesSwannn@users.noreply.github.com>
2023-02-23 18:39:42 +0000

    created seeds file and seeded app

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
   has_many :bookmarks, dependent: :restrict_with_exception
   has_many :lists, through: :bookmarks
-  validates :title, uniqueness: true
+  validates :name, uniqueness: true
   validates :overview, uniqueness: true
 end
