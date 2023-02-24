976ceb8050f9e320ccb5ca8530ea6ab42a286732
André Marques <115092696+CharlesSwannn@users.noreply.github.com>
2023-02-24 10:13:07 +0000

    created bookmarks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :lists, only: %i[index show new create] do
-    resources :bookmarks,only: %i[new create]
+    resources :bookmarks, only: %i[new create]
   end
   resources :movies
 end
