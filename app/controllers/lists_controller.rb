4fc86e82260fa7261608ef8c0367d82396557b79
André Marques <115092696+CharlesSwannn@users.noreply.github.com>
2023-02-24 10:03:52 +0000

    created lists index show and new (with simple form)

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,8 +8,22 @@ class ListsController < ApplicationController
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+    @list.save
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
